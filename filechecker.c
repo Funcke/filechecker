31efae5c91f09ef2495a813bc7868e87da0a2611
Funcke <funckejonas@gmail.com>
2016-10-01 12:53:47 +0200

    Update filechecker.c

--- filechecker.c
+++ filechecker.c
@@ -8,11 +8,11 @@ int main(void){
 	//deklarations
 	int i;//Counting variable used in for-loop looking for the '\n'
 	char line[10000];//file-path
-	char ch;
+	char ch;//
 	struct stat puffer;//file-attributes
 	printf("Welcome to the file-accesstester:\n");
 	do{
-		printf("Put in the file-path\n");
+		printf("Put in the file path\n");
 		fgets(line, 100000, stdin);
 		if(line!=NULL){//Test for empty String
 			for(i=0; line[i]!='\n'; i++){
@@ -21,16 +21,15 @@ int main(void){
 			line[i]='\0';//Killling the newline
 			//Getting target-attributes
 			if(stat(line, &puffer)==0){
-				if(puffer.st_mode & S_IWRITE){
+				if(puffer.st_mode & S_IWRITE){//write access
 					printf("You got writing-access\n");
-				}else if(puffer.st_mode & S_IREAD){
+				}else if(puffer.st_mode & S_IREAD){//read access
 					printf("You got reading-access\n");
-				}else if(puffer.st_mode & S_IEXEC){
+				}else if(puffer.st_mode & S_IEXEC){//execution access
 					printf("You got executing-access\n");
-				}else{
+				}else{//no access
 					printf("It looks like you don't got any permission for this\n"
-						"'Do'h' - Homer J. Simpson\n");
-							
+						"'Do'h' - Homer J. Simpson\n");	
 				}
 			}else{
 				printf("There was a problem with your path!\n");
